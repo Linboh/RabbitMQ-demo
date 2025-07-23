de4f76144cf4677f6e4dd5adc7c8495378e48000
LinYingXiong <632162883@qq.com>
2025-07-23 22:25:03 +0800

    消息转换器

--- consumer/src/main/java/com/itheima/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/itheima/consumer/ConsumerApplication.java
@@ -1,6 +1,7 @@
 package com.itheima.consumer;
 
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
